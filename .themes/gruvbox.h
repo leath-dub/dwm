d0c660c7c5f001c873506725c4f73c57016fdc78
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-01-28 16:26:55 +0000

    added color patch

--- .themes/gruvbox.h
+++ .themes/gruvbox.h
@@ -6,5 +6,10 @@ static const char col_cyan[]        = "#83a598";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
+    [SchemeSel]  = { col_gray4, col_gray1,  col_gray2  },
+    [SchemeStatus]  = { col_gray4, "#3c3836",  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  = { col_gray4, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
