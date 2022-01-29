c3641382e2acac3e968c4696432def23b50562b6
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-01-29 10:20:03 +0000

    commit operation

--- .themes/gruvbox.h
+++ .themes/gruvbox.h
@@ -3,6 +3,7 @@ static const char col_gray2[]       = "#d5c4a1";
 static const char col_gray3[]       = "#83a598";
 static const char col_gray4[]       = "#d5c4a1";
 static const char col_cyan[]        = "#83a598";
+
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
